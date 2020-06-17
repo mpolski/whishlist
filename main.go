c5eaa664cd9e887f8d5da40be8a49cb1f378119e
mpolski <polski@hpe.com>
2020-06-17 10:38:48 +0200

    initial commit

--- main.go
+++ main.go
@@ -12,9 +12,6 @@ import (
 	
 )
 
-
-// The store variable is a package level variable that will be available for
-// use throughout our application code
 var store Store
 
 
@@ -24,7 +21,6 @@ func main() {
 
 	//connStr format should be for example: "postgres://pqgotest:password@localhost/pqgotest?sslmode=verify-full"
 	//see https://godoc.org/github.com/lib/pq for more details
-	//connStr := "postgres://postgres_user:postgres@192.168.254.148/postgres_db"
 	connString := os.Getenv("PG_CONNSTRING")
 	db, err := sql.Open("postgres", connString)
 	if err != nil {
@@ -51,9 +47,6 @@ func helloServer(w http.ResponseWriter, r *http.Request) {
 func newRouter() *mux.Router {
 	r := mux.NewRouter()
 	r.HandleFunc("/hello", helloServer).Methods("GET")
-	//	r.HandleFunc("/presents", handlers.GetPresentHandler).Methods("GET")
-	//	r.HandleFunc("/presents", handlers.CreatePresentHandler).Methods("POST")
-
 	r.HandleFunc("/presents", GetPresentHandler).Methods("GET")
 	r.HandleFunc("/presents", CreatePresentHandler).Methods("POST")
 	r.HandleFunc("/totalbudget", GetTotalBudgetHandler).Methods("GET")
@@ -81,18 +74,13 @@ func (store *dbStore) GetPresent() ([]*Present, error) {
 	}
 	defer rows.Close()
 
-	//Create data stuctre taht is returned by the function
-	//It's empty by default
-
 	presents := []*Present{}
 
 	for rows.Next() {
-		//creates a pointer for each returned row
 		rowWithPresent := &Present{}
 		if err := rows.Scan(&rowWithPresent.Person, &rowWithPresent.Present, &rowWithPresent.Budget); err != nil {
 			return nil, err
 		}
-		//append the result to the returned array
 		presents = append(presents, rowWithPresent)
 	}
 	return presents, nil
@@ -117,38 +105,20 @@ func (store *dbStore) GetTotalBudget() ([]*TotalBudget, error) {
 	return totalBudget, nil
 }
 
-//PART OF STORE.GO 
-/*
-We will need to call the InitStore method to initialize the store. This will
-typically be done at the beginning of our application (in this case, when the server starts up)
-This can also be used to set up the store as a mock, which we will be observing
-later on
-*/
-
-//var store Store
-
 func InitStore(s Store) {
 	store = s
 }
 
-//Store defines thre methdods, to Create and to Get presents and calculate TotalBudget
 type Store interface {
 	CreatePresent(present *Present) error
 	GetPresent() ([]*Present, error)
 	GetTotalBudget() ([]*TotalBudget, error)
 }
 
-//dbStore struct implements Store interface. It takes sql.DB connections object to represent the database connection.
 type dbStore struct {
 	db *sql.DB
 }
 
-
-//PART OF HANDLERS.GO
-
-//handlers functions
-
-//GetPresentHandler used by main.go
 func GetPresentHandler(w http.ResponseWriter, r *http.Request) {
 	presents, err := store.GetPresent()
 
@@ -162,7 +132,6 @@ func GetPresentHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write(presentsListBytes)
 }
 
-//GetTotalBudgetHandler used by main.go
 func GetTotalBudgetHandler(w http.ResponseWriter, r *http.Request) {
 	totalBudget, err := store.GetTotalBudget()
 
@@ -176,7 +145,6 @@ func GetTotalBudgetHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write(totalBudgetListBytes)
 }
 
-//CreatePresentHandler used by main.go
 func CreatePresentHandler(w http.ResponseWriter, r *http.Request) {
 	present := Present{}
 
@@ -201,51 +169,12 @@ func CreatePresentHandler(w http.ResponseWriter, r *http.Request) {
 
 var presents []Present
 
-/*
-//GetPresent used by main.go
-func GetPresent(w http.ResponseWriter, r *http.Request) {
-	presentListBytes, err := json.Marshal(presents)
-	//return error by printing to console in case of error
-	if err != nil {
-		fmt.Println(fmt.Errorf("Error: %v", err))
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	//if ok, write the JSON list od dreams to the reponse
-	w.Write(presentListBytes)
-}
-
-//CreatePresent used by main.go
-func CreatePresent(w http.ResponseWriter, r *http.Request) {
-	present := Present{}
-	//parse the form values
-	err := r.ParseForm()
-	if err != nil {
-		fmt.Println(fmt.Errorf("Error: %v", err))
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	//if ok
-	present.Person = r.Form.Get("person")
-	present.Budget = r.Form.Get("budget")
-	present.Present = r.Form.Get("present")
-
-	presents = append(presents, present)
-
-	http.Redirect(w, r, "/static/", http.StatusFound)
-}
-
-*/
-
-//Present used in ...
 type Present struct {
 	Budget  string `json:"budget"`
 	Person  string `json:"person"`
 	Present string `json:"present"`
 }
 
-//TotalBudget used in ...
 type TotalBudget struct {
 	Total int64 `json:"total"`
 }
